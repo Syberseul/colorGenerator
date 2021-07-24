59e800c87031bd8b64232ad9c54f95a3db626c05
Yiteng Huang <hyy528924543@gmail.com>
2021-07-24 14:56:43 +1000

    refactor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 import "./App.css";
 
@@ -6,12 +6,28 @@ function App() {
   const [colorImg, setColorImg] = useState(``);
   const [showColorImg, setShowColorImg] = useState(false);
 
+  useEffect(() => {
+    setColorImg(
+      <div className="colorImg">
+        {generateColor().map((color, idx) => (
+          <div
+            className="colorPixel"
+            style={{
+              backgroundColor: `rgb(${color})`,
+            }}
+            key={idx}
+          ></div>
+        ))}
+      </div>
+    );
+  }, []);
+
   const colors = [
     8, 16, 24, 32, 40, 48, 56, 64, 72, 80, 88, 96, 104, 112, 120, 128, 136, 144,
     152, 160, 168, 176, 184, 192, 200, 208, 216, 224, 232, 240, 248, 256,
   ];
 
-  const generateColor = () => {
+  const generateColor = useCallback(() => {
     const colorArr = [];
     for (let red = 0; red < colors.length; red++) {
       for (let green = 0; green < colors.length; green++) {
@@ -21,33 +37,15 @@ function App() {
       }
     }
     return colorArr;
-  };
-
-  function toggleShowImg() {
-    setShowColorImg(!showColorImg);
-
-    if (colorImg === ``) {
-      setColorImg(
-        <div className="colorImg">
-          {generateColor().map((color, idx) => (
-            <div
-              className="colorPixel"
-              style={{
-                backgroundColor: `rgb(${color})`,
-              }}
-              key={idx}
-            ></div>
-          ))}
-        </div>
-      );
-    }
-  }
+  }, []);
 
   return (
     <div className="App">
-      <button onClick={toggleShowImg}>Generate color image</button>
+      <button onClick={() => setShowColorImg(!showColorImg)}>
+        {showColorImg ? "Hide Image" : "Generate Image"}
+      </button>
 
-      {showColorImg === true ? (
+      {showColorImg ? (
         <>{colorImg}</>
       ) : (
         <div className="colorImg unclick"> Click button to generate image</div>
